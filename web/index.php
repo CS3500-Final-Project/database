33b5d92962e364822e705caf5c2e7d9ca0ca2a91
Matthew <matthew.w.davis@wmich.edu>
2018-11-14 16:01:42 -0500

    db launch attempt returning values straight to index.php

--- web/index.php
+++ web/index.php
@@ -10,10 +10,42 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
+//connect to pgsl db
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+  array(
+    'pdo.server' => array(
+      'driver' => 'pgsl',
+      'user' => $dbopts["user"],
+      'password' => $dbopts["pass"],
+      'host' => $dbopts["host"],
+      'port' => $dbopts["port"],
+      'dbname' => ltrim($dbopts["path"],'/')
+    )
+  )
+);
+
+//test query
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT * FROM imgRepo');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+    echo $row . "<br>";
+  }
+
+  //return $app['twig']->render('database.twig', array(
+  //  'names' => $names
+  //));
+});
+
 // Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+//$app->register(new Silex\Provider\TwigServiceProvider(), array(
+//    'twig.path' => __DIR__.'/views',
+//));
 
 // Our web handlers
 
