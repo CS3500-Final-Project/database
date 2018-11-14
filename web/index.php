d828d211490e35a99a53d60144e162f577c27f08
Matthew <matthew.w.davis@wmich.edu>
2018-11-14 16:48:53 -0500

    sick databas

--- web/index.php
+++ web/index.php
@@ -50,7 +50,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('layout.twig'); //changed from index.twig
+  return $app['twig']->render('index.twig'); //changed from index.twig
 });
 
 $app->run();
