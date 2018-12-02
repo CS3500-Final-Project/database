14bc6066911c2f78b8ee916761072361921b48ec
Matthew <matthew.w.davis@wmich.edu>
2018-12-02 16:44:17 -0500

    curly bracket

--- web/index.php
+++ web/index.php
@@ -280,7 +280,7 @@ $app->get('/account/', function() use ($app){
 //upload redirect
 
 $app->get('/upload/', function () use ($app){
-  if( loggedIn() ){ return $app->redirect('./account-pages/html') }
+  if( loggedIn() ){ return $app->redirect('./account-pages/html'); }
   return $app->redirect('./upload.html');
 });
 
