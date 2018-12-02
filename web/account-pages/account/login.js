38e05eeab7c211e9f460ab90a1f6e64f81b12f67
Josh Weigel <jkd1703@wmich.edu>
2018-12-01 23:36:24 -0500

    added missing loading modal show/hide functions. will add remaining ones later.

--- web/account-pages/account/login.js
+++ web/account-pages/account/login.js
@@ -128,6 +128,18 @@ function onLoginButtonPressed(
 } // onLoginButtonPressed
 
 
+function showLoadingModal() {
+  modalData.currentModal = MODALS.LOADING;
+  modalData.isModalShown = true;
+
+  return;
+} // showLoadingModal
+function hideLoadingModal() {
+  modalData.isModalShown = false;
+
+  return;
+} // hideLoadingModal
+
 function showFormErrorModal() {
   modalData.currentModal = MODALS.FORM_ERROR;
   modalData.isModalShown = true;
