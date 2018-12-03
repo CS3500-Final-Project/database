54a235aacd246c2883be2893fa5bfca499cf3ea7
Matthew <matthew.w.davis@wmich.edu>
2018-12-03 12:47:40 -0500

    image viewing

--- web/image-pages/image/view.js
+++ web/image-pages/image/view.js
@@ -4,7 +4,7 @@ var account = {
   username: '',
   displayName: ''
 };
-/*
+
 var viewedImageData = {
   imageId: 0,
   uploaderUsername: '',
@@ -32,7 +32,7 @@ function deleteComment() {}
 
 makeGetRequest('/image-pages/').then(
   ( response ) => {
-    var viewedImageData = JSON.parse(response);
+    viewedImageData = JSON.parse(response);
     console.log(viewedImageData);
 
   }
