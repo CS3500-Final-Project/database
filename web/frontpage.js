e6fe1cd443f8f950bb0b98decc04c10a24e6eb15
Josh Weigel <jkd1703@wmich.edu>
2018-12-03 12:07:53 -0500

    fixing issues on front page

--- web/frontpage.js
+++ web/frontpage.js
@@ -1,73 +1,82 @@
+
+var accountData = {
+  accountId : 0,
+  displayName: ''
+};
+
 function makeGetRequest(
-    url
-  ) {
-    let promise = new Promise(
-      function(
-        resolve,
-        reject
-      ) {
-        var request = new XMLHttpRequest();
+  url
+) {
+  let promise = new Promise(
+    function(
+      resolve,
+      reject
+    ) {
+      var request = new XMLHttpRequest();
 
-        request.open(
-          "GET",
-          url
-        );
+      request.open(
+        "GET",
+        url
+      );
 
-        request.onload = function() {
-          let status = this.status;
+      request.onload = function() {
+        let status = this.status;
 
-          if (
-            status >= 200
-            && status < 300
-          ) {
-            resolve(
-              this.response
-            );
-          }
-          else {
-            reject(
-              this.statusText
-            );
-          }
-        }; // onload callback
+        if (
+          status >= 200
+          && status < 300
+        ) {
+          resolve(
+            this.response
+          );
+        }
+        else {
+          reject(
+            this.statusText
+          );
+        }
+      }; // onload callback
 
-        request.send();
-      } // executor
-    ); // promise
+      request.send();
+    } // executor
+  ); // promise
 
-    return promise;
-  } // makeGetRequest
-  var imagePreviews = [];
-  makeGetRequest('/fp/').then(
-    ( response ) => {
-      // response might be text (a JSON string)
-      // or it could be an object...
-      // I was getting JSON strings even with
-      // the headers set up to return JSON
-      let fp_images = JSON.parse(response);
-      console.log( 'received response from server' );
-      //console.log(response);
-      //testing
-      //let tiles = document.getElementById('tiles');
-      //tiles.innerHTML = '<hr></br>';
-      for(var i=0; i<fp_images.images.length; i++){
-        let imagePreview = fp_images.images[i];
-        //https://res.cloudinary.com/hws6kskjw/image/upload/v1543265795/qwh3dlfeqygdtitheijg.jpg
-        //https://res.cloudinary.com/hws6kskjw/image/upload/t_media_lib_thumb/v1543265795/qwh3dlfeqygdtitheijg.jpg
-        let urlParts = imagePreview.url.split( 'upload/' );
-        imagePreview.thumbUrl = urlParts.join( 'upload/t_media_lib_thumb/' );
-        //  tiles.innerHTML += fp_images.images[i].url + '<br>';
-        //  tiles.innerHTML += fp_images.images[i].uploaddate + '<br><br>';
-        imagePreviews.push( imagePreview );
-      }
+  return promise;
+} // makeGetRequest
+var imagePreviews = [];
+makeGetRequest('/fp/').then(
+  ( response ) => {
+    // response might be text (a JSON string)
+    // or it could be an object...
+    // I was getting JSON strings even with
+    // the headers set up to return JSON
+    let fp_images = JSON.parse(response);
+    console.log( 'received response from server' );
+    //console.log(response);
+    //testing
+    //let tiles = document.getElementById('tiles');
+    //tiles.innerHTML = '<hr></br>';
+    for(var i=0; i<fp_images.images.length; i++){
+      let imagePreview = fp_images.images[i];
+      //https://res.cloudinary.com/hws6kskjw/image/upload/v1543265795/qwh3dlfeqygdtitheijg.jpg
+      //https://res.cloudinary.com/hws6kskjw/image/upload/t_media_lib_thumb/v1543265795/qwh3dlfeqygdtitheijg.jpg
+      let urlParts = imagePreview.url.split( 'upload/' );
+      imagePreview.thumbUrl = urlParts.join( 'upload/t_media_lib_thumb/' );
+      //  tiles.innerHTML += fp_images.images[i].url + '<br>';
+      //  tiles.innerHTML += fp_images.images[i].uploaddate + '<br><br>';
+      imagePreviews.push( imagePreview );
+    }
 
-    } // response callback
-  );
+  } // response callback
+);
+
+
 var view = new Vue(
   {
     el: '#app-container',
     data: {
-      imagePreviews: imagePreviews
+      imagePreviews: imagePreviews,
+      accountData: accountData
     }
   }
 );
